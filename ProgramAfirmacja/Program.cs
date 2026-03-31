f78b5b7ae5e85357eea652edc4ce95c0fff63ca2
Michał Żukowski <s26133@pjwstk.edu.pl>
2026-03-31 20:42:27 +0200

    Dodanie buziek wzmacniających przekaz

--- ProgramAfirmacja/Program.cs
+++ ProgramAfirmacja/Program.cs
@@ -9,14 +9,48 @@
         if (liczba <= 8)
         {
             Console.WriteLine("Hello, you are a wonderful person!"); //wypisze sie 8 na 10 przypadków, losowo
+            Console.WriteLine(@"
+     .ooooo.
+   .oP""   ""bo.
+  oP'  ^   ^  `Yo
+ oP'  (o) (o)  `Yo
+oP'           `Yo
+oP'   \___/   `Yo
+oP'           `Yo
+`Yo.         .oP'
+ `Yo.       .oP'
+   `""YoooooY""' ");
         }
         else if (liczba == 9)
         {
-            Console.WriteLine("Hello, you are NOT a wonderful person!"); 
+            Console.WriteLine("Hello, you are NOT a wonderful person!");
+            Console.WriteLine(@"
+      .ooooo.
+   .oP""   ""bo.
+  oP'       `Yo
+ oP'  o   o  `Yo
+oP'           `Yo
+oP'  -------  `Yo
+oP'           `Yo
+`Yo.         .oP'
+ `Yo.       .oP'
+   `""YoooooY""'");
+
         }
         else if (liczba == 10)
         {
             Console.WriteLine("Hello, you are the worst!");
+            Console.WriteLine(@"
+      .ooooo.
+   .oP""   ""bo.
+  oP'  _   _  `Yo
+ oP'  (o) (o)  `Yo
+oP'      ^      `Yo
+oP'    .---.    `Yo
+oP'   /     \   `Yo
+`Yo.         .oP'
+ `Yo.       .oP'
+   `""YoooooY""'");
         }
     }
 }
